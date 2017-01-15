b0902ae9073bfceb7163945c4702fad7a6af3af0
Colby Griffiths <Colby Griffiths>
2017-01-14 21:36:00 -0800

    added Main & Project page incl CSS & JS source files

---
+++ web/main.js
@@ -0,0 +1,5 @@
+
+
+function press() {
+			alert("Clicked!");
+		}
---
+++ web/projects.php
@@ -0,0 +1,66 @@
+<!doctype html>
+
+<html lang="en">
+	
+	<head>
+	  
+	  <meta charset="utf-8">
+
+	  <title>CS313| Projects</title>
+	  
+	  <meta name="description" content="CS313 Colby Griffiths">
+	  
+	  <meta name="author" content="Datto-Solografix">
+
+	  <link rel="stylesheet" href="main.css">
+	  
+	  <script src="main.js"></script>
+
+	</head>
+
+	<body>
+		<nav>
+			<ul class="mainnav">
+				<li>
+					<a href="main.php">Home</a>
+				</li>
+				<li>
+					<a href="projects.php">Projects</a>
+						<!--<ul class="minornav">
+							<li><a href="#">Project1</a></li>
+							<li><a href="#">Project2</a></li>
+							<li><a href="#">Project3</a></li>
+							<li><a href="#">Project4</a></li>
+						</ul>-->
+				</li>
+				<li>
+					<a href="about.php">About Me</a>
+				</li>
+			</ul>
+		
+		</nav>
+		
+		<main>
+	  <div class="Project">
+				<h1 class="standard">Project 1</h1><h1 class="soon">Coming Soon!</h1>
+						<p>Project will be started soon</p>
+	  </div>
+	  
+	  <div class="Project">
+				<h1 class="standard">Project 2</h1><h1 class="soon">Coming Soon!</h1>		
+						<p>Project will be started soon</p>
+	  </div>
+	  
+	  <div class="Project">
+				<h1 class="standard">Project 3</h1><h1 class="soon">Coming Soon!</h1>		
+						<p>Project will be started soon</p>
+	  </div>
+	  
+	  <div class="Project">
+				<h1 class="standard">Project 4</h1><h1 class="soon">Coming Soon!</h1>	
+						<p>Project will be started soon</p>
+	  </div>
+		</main>
+	</body>
+	
+</html>
