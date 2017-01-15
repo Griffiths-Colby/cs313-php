d70b41c15b683f8bb13a5a5d2f5cdb43ca53e4db
Colby Griffiths <Colby Griffiths>
2017-01-14 21:41:52 -0800

    added About page

---
+++ web/about.php
@@ -0,0 +1,69 @@
+<!doctype html>
+
+<html lang="en">
+	
+	<head>
+	  
+	  <meta charset="utf-8">
+
+	  <title>CS313| About Me</title>
+	  
+	  <meta name="description" content="CS313 Colby Griffiths">
+	  
+	  <meta name="author" content="Datto-Solografix">
+
+	  <link rel="stylesheet" href="main.css">
+	  
+	  <script src="main.js"></script>
+
+	</head>
+
+	<body>
+		<nav>
+			<ul class="mainnav">
+				<li>
+					<a href="main.php">Home</a>
+				</li>
+				<li>
+					<a href="projects.php">Projects</a>
+						<!--<ul class="minornav">
+							<li><a href="#">Project1</a></li>
+							<li><a href="#">Project2</a></li>
+							<li><a href="#">Project3</a></li>
+							<li><a href="#">Project4</a></li>
+						</ul>-->
+				</li>
+				<li>
+					<a href="about.php">About Me</a>
+				</li>
+			</ul>
+		
+		</nav>
+	
+		<main>
+                
+			<div class="textbox">
+				<h1>About Me</h1>
+					<p><span>My Name-</span> The Company is Solografix (One graphics).  It implies that you only need one company 
+					to take care of all your website, advertising, and graphic design needs...and me? My name is Colby.</p>
+					
+					<p><span class="prompt">Who am I?</span> I am currently a student at Brigham Young University-Idaho.  I recently decided
+					to go back to school to further improve myself and to learn more about the always changing and ever improving computer 
+					technology that surrounds us everywhere today.  I am currently working towards a Bachelors degree in Computer Information Technology
+					with a minor in Web Design and Development. I will help you in any way I can.</p>
+					
+					<p><span>What are my goals?</span> I plan to incorporate much of what I am learning into the current company I work for 
+					by expanding the online presence of the business.  I will also incorporate my learning by becoming the in house information technology 
+					specialist in our company.  I would also like to learn more computer programming.  I would like to learn 
+					enough to be able to create apps for Android, Apple, and Windows based devices.</p>
+					
+					<p><span>Why do I keep saying "We"?</span> Solografix is more than just a one man show.  Even though the about me page highlights 
+					me, the founder, there are others who work with me.  Don't worry, however, the quality of work will be just like you have always come to 
+					expect from our previous interactions.</p>
+			</div>
+	
+		</main>
+	  
+	</body>
+	
+</html>
